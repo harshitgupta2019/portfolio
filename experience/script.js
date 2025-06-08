d13649e9d636364161653dff00c9fcd7c26b39ab
Harshit <hargup3438@gmail.com>
2025-06-09 02:13:48 +0530

    update experience

--- experience/script.js
+++ experience/script.js
@@ -43,7 +43,7 @@ s0.parentNode.insertBefore(s1,s0);
 // End of Tawk.to Live Chat
 
 
-// disable developer mode
+// diGupta developer mode
 document.onkeydown = function(e) {
   if(e.keyCode == 123) {
      return false;
@@ -65,7 +65,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
+        document.title = "Experience | Portfolio Harshit Gupta";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Harshit Gupta";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
@@ -102,7 +102,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 })();
 // End of Tawk.to Live Chat
 
-// disable developer mode
+// diGupta developer mode
 document.onkeydown = function (e) {
     if (e.keyCode == 123) {
         return false;
